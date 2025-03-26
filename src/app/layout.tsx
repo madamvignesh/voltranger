f7d57ad51ae7819fe2f0ce554d5fcf6e79283b24
Madam Vignesh <madamvignesh@gmail.com>
2025-03-26 15:58:46 +0530

    Adding Favicon to Website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="favicon/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png" />
+        <title>Voltranger - An off-road electrical scooter</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-900`}
       >
